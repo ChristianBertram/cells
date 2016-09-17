0aaf797c818b8e68024475581cb22af4c9b67900
Christian Bertram <cbertramdk@gmail.com>
2016-09-17 22:04:01 +0000

    Rename Rule index nubers

--- Main.hs
+++ Main.hs
@@ -8,25 +8,25 @@ import Text.Read
 
 type Line = [Bool]
 
-data Rule = Rule { rule8 :: Bool -- Color of field with neighbors True-True-True
-                 , rule7 :: Bool -- Color of field with neighbors True-True-False
-                 , rule6 :: Bool -- Color of field with neighbors True-False-True
-                 , rule5 :: Bool -- Color of field with neighbors True-False-False
-                 , rule4 :: Bool -- Color of field with neighbors False-True-True
-                 , rule3 :: Bool -- Color of field with neighbors False-True-False
-                 , rule2 :: Bool -- Color of field with neighbors False-FalseTrue
-                 , rule1 :: Bool -- Color of field with neighbors False-False-False
+data Rule = Rule { rule7 :: Bool -- Color of field with neighbors True-True-True
+                 , rule6 :: Bool -- Color of field with neighbors True-True-False
+                 , rule5 :: Bool -- Color of field with neighbors True-False-True
+                 , rule4 :: Bool -- Color of field with neighbors True-False-False
+                 , rule3 :: Bool -- Color of field with neighbors False-True-True
+                 , rule2 :: Bool -- Color of field with neighbors False-True-False
+                 , rule1 :: Bool -- Color of field with neighbors False-FalseTrue
+                 , rule0 :: Bool -- Color of field with neighbors False-False-False
                  } deriving Show
 
 ruleGet :: Int -> Rule -> Bool
-ruleGet 7 = rule8
-ruleGet 6 = rule7
-ruleGet 5 = rule6
-ruleGet 4 = rule5
-ruleGet 3 = rule4
-ruleGet 2 = rule3
-ruleGet 1 = rule2
-ruleGet 0 = rule1
+ruleGet 7 = rule7
+ruleGet 6 = rule6
+ruleGet 5 = rule5
+ruleGet 4 = rule4
+ruleGet 3 = rule3
+ruleGet 2 = rule2
+ruleGet 1 = rule1
+ruleGet 0 = rule0
 ruleGet _ = const False
 
 boolToInt :: Bool -> Int
@@ -79,14 +79,14 @@ main = do
   its <- getInt 0 maxBound
   putStr "\nEnter a rule from 0 to 255: "
   ruleInt <- getInt 0 255
-  let rule = Rule { rule8 = intToBool $ div (mod ruleInt 256) 128
-                  , rule7 = intToBool $ div (mod ruleInt 128) 64
-                  , rule6 = intToBool $ div (mod ruleInt 64) 32
-                  , rule5 = intToBool $ div (mod ruleInt 32) 16
-                  , rule4 = intToBool $ div (mod ruleInt 16) 8
-                  , rule3 = intToBool $ div (mod ruleInt 8) 4
-                  , rule2 = intToBool $ div (mod ruleInt 4) 2
-                  , rule1 = intToBool $ mod ruleInt 2
+  let rule = Rule { rule7 = intToBool $ div (mod ruleInt 256) 128
+                  , rule6 = intToBool $ div (mod ruleInt 128) 64
+                  , rule5 = intToBool $ div (mod ruleInt 64) 32
+                  , rule4 = intToBool $ div (mod ruleInt 32) 16
+                  , rule3 = intToBool $ div (mod ruleInt 16) 8
+                  , rule2 = intToBool $ div (mod ruleInt 8) 4
+                  , rule1 = intToBool $ div (mod ruleInt 4) 2
+                  , rule0 = intToBool $ mod ruleInt 2
                   }
 
   let firstLine = replicate (its-1) False ++ [True] ++ replicate (its-1) False
